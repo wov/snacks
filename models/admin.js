a933766bd4965ddcfeb2e1bc487a428ac758e45d
LenyeMeng <lenyemeng@anjukeinc.com>
2013-11-09 15:19:26 +0800

    add addInfo method

--- models/admin.js
+++ models/admin.js
@@ -4,6 +4,7 @@ var connection = mysql.createConnection({
 	host: '192.168.1.101', 
 	user: 'snacks',
 	password: 'snacks',
+	database: 'snacks',
 });
 
 
@@ -11,6 +12,19 @@ function addInfo(appInfo) {
 	connection.connect();
 	console.log("Add AppInfo:");
 	console.log(appInfo);
+	query = "insert into applist(name,category_id,icon_id,description,create_time,update_time) values( \
+		'" + appInfo.name + "', \
+		'" + appInfo.category_id + "', \
+		" + appInfo.icon_id + ", \
+		'" + appInfo.description + "', \
+		" + appInfo.create_time + ", \
+		" + appInfo.update_time + " \
+		);";
+	console.log(query);
+	connection.query(query, function (err, rows, fields) {
+		if (err) { throw err; };
+		console.log("This query is affected ");
+	});
 	connection.end();
 }
 
--- routes/appManage.js
+++ routes/appManage.js
@@ -12,11 +12,8 @@ exports.index = function(req, res){
 
 exports.addAppInfo = function (req, res) {
     info = req.body;
-    console.log(info);
-    var appInfo = {"name":"new app"};
-    adminModel.addInfo(appInfo);
-	// res.send('this is appmanage');
-	// res.render('appmanager');
-	// res.send(req.body);
-
+    info.icon_id = 1;
+    info.create_time = info.update_time = Date.parse(new Date())/1000;
+    adminModel.addInfo(info);
+	
 }
