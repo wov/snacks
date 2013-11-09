0dff430e94b69bddf30c6fd93be61aed7dd46a6c
zjh <zjhsdtc@gmail.com>
2013-11-09 14:45:32 +0800

    create upload jade

--- routes/appManage.js
+++ routes/appManage.js
@@ -5,13 +5,18 @@ exports.index = function(req, res){
     // res.send('this is appmanage');
     // res.render('appmanager');
     // res.send(req.body);
-    info = req.body;
+	res.render('appmanager');
+
 
 }
 
 exports.addAppInfo = function (req, res) {
-
+    info = req.body;
+    console.log(info);
     var appInfo = {"name":"new app"};
     adminModel.addInfo(appInfo);
+	// res.send('this is appmanage');
+	// res.render('appmanager');
+	// res.send(req.body);
 
 }
