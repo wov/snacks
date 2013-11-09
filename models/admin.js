80bcfbeaf7d2c01635266546c62b069a002ea1ed
LenyeMeng <lenyemeng@anjukeinc.com>
2013-11-09 14:11:49 +0800

    #!

--- models/admin.js
+++ models/admin.js
@@ -1,5 +1,17 @@
-function addInfo(AppInfo) {
-	return "hello";
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: '192.168.1.101', 
+	user: 'snacks',
+	password: 'snacks',
+});
+
+
+function addInfo(appInfo) {
+	connection.connect();
+	console.log("Add AppInfo:");
+	console.log(appInfo);
+	connection.end();
 }
 
 exports.addInfo = addInfo;
